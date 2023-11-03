a0bbc495c79be2cb3b02043c7f11ffe4f4de2f4f
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-11-03 01:48:27 +0000

    feat: thread mention function

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,6 +1,21 @@
 module.exports = async (client, message) => {
 	if (message.guild === null || message.author.bot) return;
 
+	if (message.channel.parentId === '1169708188493435000') {
+		const userIdPattern = /(\d{17,19})/g;
+		const mentionedUserIds = message.content.match(userIdPattern);
+
+		if (mentionedUserIds) {
+			mentionedUserIds.forEach(userId => {
+				client.channels.cache
+					.get('1169793238350180432')
+					.send(
+						`ID do usuário mencionado: ${userId}\n\nThread: ${message.channel}`,
+					);
+			});
+		}
+	}
+
 	if (
 		message.content.startsWith('fp?') &&
 		(message.author.id !== '354233941550694400' ||
