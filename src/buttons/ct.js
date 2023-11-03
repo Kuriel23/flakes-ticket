d5ec944da529599909488a64951d83260c04897d
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-11-03 02:16:25 +0000

    feat: button for only mods

--- src/buttons/ct.js
+++ src/buttons/ct.js
@@ -7,13 +7,17 @@ module.exports = async (client, interaction) => {
 			.setLabel('Confirmo minha escolha')
 			.setStyle(discord.ButtonStyle.Primary),
 	);
+	const requiredRoles = ['494205309225795585', '485488846990213121', '1169697807347761263'];
+
+	const hasRequiredRole = requiredRoles.some(roleId => interaction.member.roles.cache.has(roleId));
+
+	if (!hasRequiredRole)
+		return interaction.reply({ content: 'Desculpe, você não tem o cargo necessário para usar esta ação.', ephemeral: true });
+
 	interaction
 		.reply({
 			content: `Gostaria de confirmar o encerramento do seu ticket?`,
 			components: [button],
-		})
-		.then(msg => {
-			setInterval(() => msg.delete(), 45000);
 		});
 };
 
