d8a541a98c630c9cd03677b913e068a286eafe99
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-11-03 15:28:30 +0000

    refactor: grammar errors

--- src/messages/embticket.js
+++ src/messages/embticket.js
@@ -23,7 +23,7 @@ module.exports = async (client, message) => {
 		.setColor(client.cor)
 		.setTitle('Central de atendimento - Canal Flakes Power')
 		.setDescription(
-			`Se você tiver alguma dúvida, pergunta ou sugestão, selecione "Suporte" aqui em abaixo para entrar em contato com a equipe!\n\nSe você tiver ganhado alguma premiação em algum evento, selecione "Premiação" aqui no menu abaixo para resgatar!`,
+			`Se você tiver alguma dúvida, pergunta ou sugestão, selecione "Suporte" aqui abaixo para entrar em contato com a equipe!\n\nSe você tiver ganhado alguma premiação em algum evento, selecione "Premiação" aqui no menu abaixo para resgatar!`,
 		)
 		.setThumbnail("https://cdn.discordapp.com/attachments/1095010254439333928/1169809869893800036/APkrFKaxwyhL1GHo0vS4h4YIW6YEZhvafGqDmFRxeyMLOQs900-c-k-c0x00ffffff-no-rj.png");
 	message.channel.send({ embeds: [embed], components: [row] });
